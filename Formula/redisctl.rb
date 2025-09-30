c97371b7843f5cfadf8b2de0d60cedca36e9fc5f
Josh Rotenberg <joshrotenberg@gmail.com>
2025-09-30 15:43:37 -0700

    feat: add redisctl formula

---
+++ Formula/redisctl.rb
@@ -0,0 +1,17 @@
+class Redisctl < Formula
+  desc "Unified CLI tool for managing Redis Cloud and Redis Enterprise deployments"
+  homepage "https://github.com/joshrotenberg/redisctl"
+  url "https://github.com/joshrotenberg/redisctl/archive/refs/tags/v0.6.2.tar.gz"
+  sha256 "5a611eca1ff7639c1e88af6b0dd64d631aa0f8f61accff24b3f5aef2a0ee457f"
+  license any_of: ["MIT", "Apache-2.0"]
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "redisctl", shell_output("#{bin}/redisctl --version")
+  end
+end
