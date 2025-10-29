d9460111e083966ce7518788e29b0876c5bd3403
Josh Rotenberg <joshrotenberg@gmail.com>
2025-10-29 15:30:49 -0700

    redisctl 0.6.6

--- Formula/redisctl.rb
+++ Formula/redisctl.rb
@@ -1,24 +1,24 @@
 class Redisctl < Formula
   desc "Unified CLI tool for managing Redis Cloud and Redis Enterprise deployments"
   homepage "https://github.com/joshrotenberg/redisctl"
-  version "0.6.2"
+  version "0.6.6"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
-      url "https://github.com/joshrotenberg/redisctl/releases/download/v0.6.2/redisctl-aarch64-apple-darwin.tar.xz"
-      sha256 "47982ee2ad900c695890be68978cceeb65d628ae2fbb553f248918f309b595a6"
+      url "https://github.com/joshrotenberg/redisctl/releases/download/redisctl-v0.6.6/redisctl-aarch64-apple-darwin.tar.xz"
+      sha256 "c2e5bdfd36d30842a104e6439391ac63987c7d2307cf26e4351da99254f1e47a"
     end
     on_intel do
-      url "https://github.com/joshrotenberg/redisctl/releases/download/v0.6.2/redisctl-x86_64-apple-darwin.tar.xz"
-      sha256 "d54236b32bfa1547ae079a54f6eff52924d69c35c887beb1c08606467249d80e"
+      url "https://github.com/joshrotenberg/redisctl/releases/download/redisctl-v0.6.6/redisctl-x86_64-apple-darwin.tar.xz"
+      sha256 "72ae0367429fd9e8c2f1718d806ea4ed2477058c5143419276da1e3b651a04db"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/joshrotenberg/redisctl/releases/download/v0.6.2/redisctl-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "8b6756048991ba7c3cacde0bf9c972eb6d783572622467314ef9d863fb143325"
+      url "https://github.com/joshrotenberg/redisctl/releases/download/redisctl-v0.6.6/redisctl-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "2f84884e3a59fec3eb589f5b9c4f6906e74c320fa9901a8e93ccf8ff99be8602"
     end
   end
 
